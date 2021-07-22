15e7c4605a6967131c178d3cfab943b76d677035
4ions <leovalenciasantana@gmail.com>
2021-07-22 15:50:15 -0500

    Changed all script

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" Module of lock boxes """
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
+"""
 
 
 def addKeys(keyInBox, keys):
@@ -13,20 +17,32 @@ def addKeys(keyInBox, keys):
     return keys
 
 
+def _canUnlockAll(boxes, keys, index):
+    """ check each box to find keys """
+    if index not in keys:
+        return keys
+    count = 0
+    count2 = 0
+    no_in_key = []
+    for box in range(0, len(boxes)):
+        if box in keys:
+            keys = addKeys(boxes[box], keys)
+        if box not in keys:
+            no_in_key.append(box)
+    for no in no_in_key:
+        if no in keys:
+            _canUnlockAll(boxes, keys, box)
+    return keys
+
+
 def canUnlockAll(boxes):
-    """
-    unlock boxes
-    """
+    """ function to unlock boxes with keys """
     if len(boxes) == 0:
         return False
     keys = []
     keys = addKeys(boxes[0], keys)
-    count = 0
-    for box in range(0, len(boxes)):
-        if box in keys:
-            keys = addKeys(boxes[box], keys)
-            count = count + 1
-    if count == len(boxes):
+    count = _canUnlockAll(boxes, keys, 0)
+    if len(keys) == len(boxes):
         return True
     else:
         return False
