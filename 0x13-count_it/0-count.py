b42198039a878aa0951ebbc541daa406eb832ddc
4ions <leovalenciasantana@gmail.com>
2022-05-05 21:35:40 -0500

    Fixed

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python3
-"""
-queries the Reddit API, parses the title of all hot articles,
-and prints a sorted count of given keywords
-"""
+"""This module contains the count_words function"""
 
 import requests
 
 
-def get_articles(subreddit, word_list, words_dict, words_dict_r, after="",):
+def get_articles(sub, w, wd, wd_r, a="",):
     """ Get articles """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    url += "?limit=100&after={}".format(after)
-    response = requests.get(url,
-                            allow_redirects=False,
-                            headers={'User-agent': 'Hb-pc'}
-                            )
+    url = "https://www.reddit.com/r/{}".format(sub)
+    url = "{}/hot.json?limit=100&after={}".format(url, a)
+    response = requests.get(url, allow_redirects=False,
+                            headers={'User-agent': 'Holberton'})
     if response.status_code != 200:
         return None
-    articles = response.json().get("data").get("children")
+    articles = response.json()["data"]["children"]
     for article in articles:
-        title_list = article.get("data").get("title").lower().split(" ")
-        for title in title_list:
-            if title in words_dict:
-                words_dict[title] += 1
-    after = response.json().get("data").get("after")
-    if after is None:
-        sorted_w = sorted(words_dict.items(), key=lambda t: t[::-1])
-        sorted_w_desc = sorted(sorted_w, key=lambda tup: tup[1], reverse=True)
-        for w in sorted_w_desc:
+        titles = article["data"]["title"].lower().split(" ")
+        for title in titles:
+            if title in wd:
+                wd[title] += 1
+    if response.json()["data"]["after"] is None:
+        sw = sorted(wd.items(), key=lambda t: t[::-1])
+        sw_desc = sorted(sw, key=lambda tup: tup[1], reverse=True)
+        for w in sw_desc:
             if w[1] > 0:
-                print("{}: {}".format(w[0], w[1] * words_dict_r[w[0]]))
+                print("{}: {}".format(w[0], w[1] * wd_r[w[0]]))
         return
-    return get_articles(subreddit, word_list, words_dict, words_dict_r, after)
+    return get_articles(sub, w, wd, wd_r, response.json()["data"]["after"])
 
 
 def count_words(subreddit, word_list):
     """ Count words """
-    words_dict = {}
-    words_dict_r = {}
+    w_d = {}
+    w_d_r = {}
     word_list = [word.lower() for word in word_list]
     for w in word_list:
-        if w not in words_dict:
-            words_dict_r[w] = 1
-            words_dict[w] = 0
+        if w not in w_d:
+            w_d_r[w] = 1
+            w_d[w] = 0
         else:
-            words_dict_r[w] += 1
-    results = get_articles(subreddit, word_list, words_dict, words_dict_r)
+            w_d_r[w] += 1
+    get_articles(subreddit, word_list, w_d, w_d_r)
