27465cde63d41f0cefb642595c7f4d32ce6a32b8
4ions <leovalenciasantana@gmail.com>
2022-05-05 21:46:50 -0500

    Fixed

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,53 +1,38 @@
 #!/usr/bin/python3
-"""
-Count it!
-"""
-from requests import request
+"""  prints a sorted count of given keywords """
+import requests
 
 
-def generate_dicts(word_list):
-    """
-    generate_dicts functions
-    """
-    count = {k: 0 for k in word_list}
-    dup = {}
-    for k in word_list:
-        if k not in dup:
-            dup[k] = 0
-        dup[k] += 1
-    return (count, dup)
-
-
-def count_words(subreddit, word_list, after="", count={}, dup={}, init=0):
-    """
-    count_words function
-    """
-    if not init:
-        count, dup = generate_dicts(word_list)
-
-    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
-    headers = {"User-Agent": "Python3"}
-    response = request("GET", url, headers=headers).json()
-    try:
-        data = response.get('data')
-        top = data.get('children')
-        _after = data.get('after')
-
-        for item in top:
-            data = item.get('data')['title']
-            for word in count:
-                amount = data.lower().split(' ').count(word.lower())
-                count[word] += amount
-
-        if _after:
-            count_words(subreddit, word_list, _after, count, dup, 1)
-        else:
-            sort_abc = sorted(count.items(), key=lambda tup: tup[::-1])
-            desc = sorted(sort_abc, key=lambda tup: tup[1], reverse=True)
-
-            for name, cnt in desc:
-                cnt *= dup[name]
-                if cnt:
-                    print('{}: {}'.format(name.lower(), cnt))
-    except Exception:
+def count_words(subreddit, word_list, counter={}, after=""):
+    """ queries the Reddit API, parses the title of all hot articles,
+    and prints a sorted count of given keywords """
+    if len(counter) <= 0:
+        for item in word_list:
+            counter[item] = 0
+    if after is None:
+        word_list = [[key, value] for key, value in counter.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for i in word_list:
+            if i[1]:
+                print("{}: {}".format(i[0].lower(), i[1]))
         return None
+    url = "https://api.reddit.com/r/{}/hot".format(subreddit)
+    parameters = {'limit': 100, 'after': after}
+    head = {'user-agent': 'my-app/0.0.1'}
+    call = requests.get(
+        url,
+        headers=head,
+        params=parameters,
+        allow_redirects=False,
+    )
+    if call.status_code == 200:
+        after = call.json().get('data').get('after')
+        children = call.json().get('data').get('children')
+        for child in children:
+            title = child.get('data').get('title')
+            lower = [j.lower() for j in title.split(' ')]
+            for word in word_list:
+                counter[word] += lower.count(word.lower())
+    else:
+        return None
+    count_words(subreddit, word_list, counter, after)
