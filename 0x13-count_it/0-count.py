b575c6337cebc768d75355934baadd96bd635edb
4ions <leovalenciasantana@gmail.com>
2022-05-05 21:33:53 -0500

    Fixed

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,53 +1,48 @@
 #!/usr/bin/python3
 """
-Count it!
+queries the Reddit API, parses the title of all hot articles,
+and prints a sorted count of given keywords
 """
-from requests import request
+
+import requests
 
 
-def generate_dicts(word_list):
-    """
-    generate_dicts functions
-    """
-    count = {k: 0 for k in word_list}
-    dup = {}
-    for k in word_list:
-        if k not in dup:
-            dup[k] = 0
-        dup[k] += 1
-    return (count, dup)
-
-
-def count_words(subreddit, word_list, after="", count={}, dup={}, init=0):
-    """
-    count_words function
-    """
-    if not init:
-        count, dup = generate_dicts(word_list)
-
-    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
-    headers = {"User-Agent": "Python3"}
-    response = request("GET", url, headers=headers).json()
-    try:
-        data = response.get('data')
-        top = data.get('children')
-        _after = data.get('after')
-
-        for item in top:
-            data = item.get('data')['title']
-            for word in count:
-                amount = data.lower().split(' ').count(word.lower())
-                count[word] += amount
-
-        if _after:
-            count_words(subreddit, word_list, _after, count, dup, 1)
-        else:
-            sort_abc = sorted(count.items(), key=lambda tup: tup[::-1])
-            desc = sorted(sort_abc, key=lambda tup: tup[1], reverse=True)
-
-            for name, cnt in desc:
-                cnt *= dup[name]
-                if cnt:
-                    print('{}: {}'.format(name.lower(), cnt))
-    except Exception:
+def get_articles(subreddit, word_list, words_dict, words_dict_r, after="",):
+    """ Get articles """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url += "?limit=100&after={}".format(after)
+    response = requests.get(url,
+                            allow_redirects=False,
+                            headers={'User-agent': 'Hb-pc'}
+                            )
+    if response.status_code != 200:
         return None
+    articles = response.json().get("data").get("children")
+    for article in articles:
+        title_list = article.get("data").get("title").lower().split(" ")
+        for title in title_list:
+            if title in words_dict:
+                words_dict[title] += 1
+    after = response.json().get("data").get("after")
+    if after is None:
+        sorted_w = sorted(words_dict.items(), key=lambda t: t[::-1])
+        sorted_w_desc = sorted(sorted_w, key=lambda tup: tup[1], reverse=True)
+        for w in sorted_w_desc:
+            if w[1] > 0:
+                print("{}: {}".format(w[0], w[1] * words_dict_r[w[0]]))
+        return
+    return get_articles(subreddit, word_list, words_dict, words_dict_r, after)
+
+
+def count_words(subreddit, word_list):
+    """ Count words """
+    words_dict = {}
+    words_dict_r = {}
+    word_list = [word.lower() for word in word_list]
+    for w in word_list:
+        if w not in words_dict:
+            words_dict_r[w] = 1
+            words_dict[w] = 0
+        else:
+            words_dict_r[w] += 1
+    results = get_articles(subreddit, word_list, words_dict, words_dict_r)
